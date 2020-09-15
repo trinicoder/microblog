f53c2d346f510ab207450a00e9e3343f53159aad
Varoon Silochan <varoonx@gmail.com>
2020-09-15 11:36:34 -0400

    wtf

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-
+from app import routes, models, errors
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
