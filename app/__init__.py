f5c0048080d3bd1ff3f4d000478c81c43a9905d7
Varoon Silochan <varoonx@gmail.com>
2020-09-15 11:32:13 -0400

    Errors

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+from app import routes, models
+from app import routes, models, errors
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -6,9 +8,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
-db =SQLAlchemy(app)
-migrate = Migrate(app,db)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
-
-from app import routes, models
